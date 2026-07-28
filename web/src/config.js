2838d0662fb1d636914b7da688f34c64650715bb
Orly <17107466+Orlyi@user.noreply.gitee.com>
2026-07-28 15:43:44 +0800

    remove web/ source from lastMusic branch

--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,5 @@
 // 开发用 localhost，打包 APK 前改成服务器地址
-const API_HOST = 'http://localhost:7999'
-// 'http://44.99.50.241:8000'
+// const API_HOST = 'http://localhost:7999'
+const API_HOST = 'http://47.99.50.241:7999'
 export const API_BASE = `${API_HOST}/api/v1`
 export const STATIC_BASE = API_HOST
